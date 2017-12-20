a2bc20fddf93a62e9119ce8203427b2aa94b1b7a
Kekoa Wilson <kekoawilson20@gmail.com>
2017-12-20 14:38:20 -0700

    first/last commit

--- js/products/productsSrvc.js
+++ js/products/productsSrvc.js
@@ -1,3 +1,5 @@
-angular.module('myApp').service('productsSrvc', function() {
+angular.module('myApp').service('productsSrvc', function($http) {
+    this.getShoeData = () => $http.get('https://practiceapi.devmountain.com/products?category=shoes')
 
+    this.getSockData = () => $http.get(`https://practiceapi.devmountain.com/products?category=socks`)
 });
